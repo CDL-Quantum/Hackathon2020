a067216c3afea1dcf6cb29b7759e3af266440554
Milan Leonard <milan.leonard@gmail.com>
2020-07-23 16:38:08 +1000

    finished QNG, will start a jupyter notebook in order to make a tutorial

--- QBC/quantum_aware_optims.py
+++ QBC/quantum_aware_optims.py
@@ -2,6 +2,7 @@
 from torch.optim import Optimizer
 from torch.autograd import Function
 from collections.abc import Iterable
+import pennylane as qml
 import torch
 torch.halfpi = torch.acos(torch.zeros(1)).item() # no built-in torch.pi, this is pi/2
 # %%
@@ -17,20 +18,24 @@ def evaluate_circuits(params, qcircuits):
             circuit_evals.append(circuit_eval)
     return circuit_evals
 
-def cost(circuit_evals, proportions):
+def costfunc(circuit_evals, proportions):
     """Takes the expectation values of the circuit evaluations and adds them according to the proportion
     Arguments:
         circuit_evals :[Float]: the expectation values
         proportions :[Float]: the mixing coefficients
     """
-    cost = 0
+    loss = 0
     for circ_eval, prop in zip(circuit_evals,proportions):
-        cost += circ_eval * prop
-    return cost
+        loss += circ_eval * prop
+    return loss
+
+def kronecker(matrix1, matrix2):
+    """ Torch kronecker product """
+    return torch.ger(matrix1.view(-1), matrix2.view(-1)).reshape(*(matrix1.size() + matrix2.size())).permute([0, 2, 1, 3]).reshape(matrix1.size(0) * matrix2.size(0), matrix1.size(1) * matrix2.size(1))
 
 class RotoSolve(Optimizer):
     """Implements the RotoSolve parameter finding algorithm for a given circuit ansatz as given by Ostaszewski et al. (2019) (https://arxiv.org/abs/1905.09692)
-    
+    QCircuits can be extended as native pytorch operations, requires QCircuit definition as pennylane QNodes. 
     Arguments:
         params (torch.Tensor): Object of :class: torch.Tensor of parameters to update
     """
@@ -40,7 +45,7 @@ class RotoSolve(Optimizer):
         self.proportions = proportions
         self.final_params = params.tolist()
         init_evals = evaluate_circuits(self.final_params, qcircuits)
-        initial_cost = cost(init_evals, proportions)
+        initial_cost = costfunc(init_evals, proportions)
         self.losses = [initial_cost]
 
         
@@ -52,15 +57,15 @@ class RotoSolve(Optimizer):
             for idx in range(len(params_)):
                 params_[idx] = 0
                 circuit_evals = torch.Tensor(evaluate_circuits(params_, self.qcircuits))
-                cost0 = cost(circuit_evals, self.proportions)
+                cost0 = costfunc(circuit_evals, self.proportions)
                 
                 params_[idx] = torch.halfpi
                 circuit_evals = torch.Tensor(evaluate_circuits(params_, self.qcircuits))
-                costplus = cost(circuit_evals, self.proportions)
+                costplus = costfunc(circuit_evals, self.proportions)
                 
                 params_[idx] = -torch.halfpi
                 circuit_evals = torch.Tensor(evaluate_circuits(params_, self.qcircuits))
-                costminus = cost(circuit_evals, self.proportions)
+                costminus = costfunc(circuit_evals, self.proportions)
 
                 a = torch.atan2(
                     2.0 * cost0 - costplus - costminus, costplus - costminus
@@ -70,11 +75,33 @@ class RotoSolve(Optimizer):
                     params_[idx] += 4+torch.halfpi
                 group['params'] = [torch.FloatTensor(params_)]
         step_circuit_evals = evaluate_circuits(params_, self.qcircuits)
-        cost = cost(step_circuit_evals, self.proportions)
+        cost = costfunc(step_circuit_evals, self.proportions)
         self.losses.append(cost)
         self.final_params = params_
         return cost
 
 
+class QuantumNaturalGradientOptim(Optimizer):
+    """ The quantum natural gradient optimizer -- assume access to TorchLayer"""
+    def __init__(self, params, qcircuit, lr):
+        self.qcircuit = qcircuit
+        super().__init__(params, defaults = {})
+        self.lr = lr
+    
+    def step(self):
+        for group in self.param_groups:
+            params_tensor = group['params'][0]
+            fubinimetric = torch.tensor(self.qcircuit.metric_tensor([[],params_tensor.tolist()])).float() # uses the fubinimetric built in
+            for p in group['params']:             
+                if p.grad is not None:
+                    natural_grad = fubinimetric@p.grad
+                    p.data.add_(-natural_grad*self.lr)
+
+    # Basically, the trick is to compute gdagger on the parameter update step.
+    
+
 
 # %%
+"""
+I want to take in the torch.tensor matrix corresponding to the parameterised quantum circuit and then apply it to the input vector 
+"""
--- QBC/test.py
+++ QBC/test.py
@@ -1,6 +1,7 @@
 import pennylane as qml
 from quantum_aware_optims import *
 import torch
+from torch.optim import SGD
 
 n_wires = 2
 
@@ -12,7 +13,7 @@ def ansatz(params):
     qml.CNOT(wires=[0, 1])
 
 @qml.qnode(dev)
-def circuit(params):
+def circuit(inputs, params):
     ansatz(params)
     return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliY(1))
 
@@ -23,9 +24,25 @@ def circuit2(params):
 
 qcircuits = [circuit, circuit2]
 proportions = torch.Tensor([0.5, 1.2, -0.2])
-init_params = torch.Tensor([2.429, 0.25])
+init_params = torch.Tensor([0.1, 0.25])
 
-optim = RotoSolve(init_params,qcircuits,proportions)
-loss = optim.step()
-print(loss)
-print(optim.param_groups)
+qlayer = qml.qnn.TorchLayer(circuit,{"params":2})
+
+expvals = qlayer(init_params)
+def loss_func(expvals):
+    loss = torch.mul(expvals[0],torch.tensor(0.5)) + torch.mul(expvals[1],torch.tensor(0.2))
+    return loss
+
+optim =  SGD([qlayer.params], lr=0.1)
+optimQNG = QuantumNaturalGradientOptim([qlayer.params],circuit, lr=0.1)
+for i in range(100):
+    output = qlayer(qlayer.params)
+    loss = loss_func(output)
+    print(loss)
+    loss.backward()
+    optimQNG.step()
+    optimQNG.zero_grad()
+# optim = RotoSolve(init_params,qcircuits,proportions)
+# loss = optim.step()
+# print(loss)
+# print(optim.param_groups)
